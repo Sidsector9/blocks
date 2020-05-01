a692eaf7c20969fc7fb9437db8709582e395d530
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2020-05-01 17:02:07 +0530

    tool: style-loader and sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require('path');
 
 module.exports = {
-	entry: './app/blocks/js/_index.jsx',
+	entry: './app/blocks/_index.jsx',
 	output: {
 		path: path.resolve( __dirname, 'dist' ),
 		filename: 'js/newsuk-blocks.min.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader',
+				]
+			}
+		]
 	}
 };
