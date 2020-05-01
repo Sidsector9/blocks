d69ee9dc0750cbbfee680a220c9c2f9f3b87f4d3
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2020-05-01 15:46:32 +0530

    feat: scaffold plugin

---
+++ newsuk-blocks.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Newsuk Blocks
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     newsuk-blocks
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Newsuk_Blocks
+ */
+
+// Your code starts here.
