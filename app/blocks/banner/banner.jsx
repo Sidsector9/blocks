f72e1fa84e00de557280e4b9c8b5084a3bc3a0e8
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2020-05-02 20:12:28 +0530

    feat: conditional banner text position

--- app/blocks/banner/banner.jsx
+++ app/blocks/banner/banner.jsx
@@ -117,23 +117,27 @@ registerBlockType( 'newsuk/banner', {
 							onChange={ ( value ) => setAttributes( { bannerSubTitle: value } ) }
 						/>
 
-						<p>Banner text position</p>
-						<RadioControl
-							className="newsuk__banner-text-positions"
-							selected={ bannerTextPosition }
-							options={ [
-								{ label: '', value: '1' },
-								{ label: '', value: '2' },
-								{ label: '', value: '3' },
-								{ label: '', value: '4' },
-								{ label: '', value: '5' },
-								{ label: '', value: '6' },
-								{ label: '', value: '7' },
-								{ label: '', value: '8' },
-								{ label: '', value: '9' },
-							] }
-							onChange={ ( value ) => { setAttributes( { bannerTextPosition: value } ) } }
-						/>
+						{ ( bannerTitle || bannerSubTitle ) && (
+							<>
+								<p>Banner text position</p>
+								<RadioControl
+									className="newsuk__banner-text-positions"
+									selected={ bannerTextPosition }
+									options={ [
+										{ label: '', value: '1' },
+										{ label: '', value: '2' },
+										{ label: '', value: '3' },
+										{ label: '', value: '4' },
+										{ label: '', value: '5' },
+										{ label: '', value: '6' },
+										{ label: '', value: '7' },
+										{ label: '', value: '8' },
+										{ label: '', value: '9' },
+									] }
+									onChange={ ( value ) => { setAttributes( { bannerTextPosition: value } ) } }
+								/>
+							</>
+						) }
 					</PanelBody>
 				</InspectorControls>
 				<div className={ className } style={ {
